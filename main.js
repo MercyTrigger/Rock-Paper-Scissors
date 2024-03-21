2b09e5809fa16b25aafcbed21795f2c76d080a74
MercyTrigger <{151790852}+{MercyTrigger}@users.noreply.github.com>
2024-03-21 17:43:21 +0300

    Add UI to my rock, paper, scissors game

--- main.js
+++ main.js
@@ -1,35 +1,29 @@
-// writing function that suppose to store computer's input data and return 
-// randomly ‘Rock’, ‘Paper’ or ‘Scissors’
-function getComputerChoice () {
-    const container = 'Rock Paper Scissors';  // choices
-    let splitString = container.split(' '); // makes an array
-    let resultComputer = Math.floor(Math.random() * splitString.length); // getting random between 0 and 2
-    let caseInsasitive = splitString[resultComputer] // returning the string result
-    return caseInsasitive.toLowerCase() // makingn it case-insensitive
-}
+function getComputerChoice() {
+    const container = ["rock", "paper", "scissors"];
+    let resultComputer = Math.floor(Math.random() * container.length); // getting random between 0 and 2
+    let randomChoice = container[resultComputer]; 
+    return randomChoice;
+};
 
-// this function chooses the winner of a game
-
-function winnerOfTheGame (player, computer) {
+function winnerOfTheGame(player, computer) {
     if (player > computer) {
-        return `Player has won the game: ${player}`
+        return `Player has won the game: ${player} against computer: ${computer}`;
     }
     else if (computer > player) {
-        return `Computer has won the game: ${computer}`
+        return `Computer has won the game: ${computer} against player: ${player}`;
     }
     else if (player === computer) {
-        return `No one has won, a draw!`
-    }
-}
+        return `No one has won, a draw! ${player} : ${computer}`;
+    };
+};
 
-// these are global scope variables to update them after each iteration in the for loop
-let player = Number();
-let computer = Number();
+let player = 0;
+let computer = 0;
 
 // this function takes 2 parametors which are user's choice via prompt (look futher in the code)
 // and computer random selection, if here's no valid input it will throw else statement
 
-function oneRoundOfGame (playerSelection, computerSelection) {
+function oneRoundOfGame(playerSelection, computerSelection) {
 
     if (playerSelection === 'rock' && computerSelection === 'paper') {
         
@@ -81,24 +75,62 @@ player: ${++player} vs  computer: ${computer}`)
     else {
          return (`Please, enter valid data!`) 
     }
-
 }
 
 
+// event listeners here
+const userChoiceDiv = document.querySelector('.user');
+const computerButton = document.querySelector('.container')
 
+computerButton.addEventListener('mouseover', () => {
+    let visiablePop = document.querySelector('.pop-up');
+    visiablePop.style.opacity = '1'
+    visiablePop.style.transition = '1s'
 
-// this is a loop function to play a game 6 times in a row, it also return winner function as return
-function game() {
-    for (let i = 0; i < 6; i++) {
-        let userChoice = prompt('Choose: rock, paper or scissors: ');
-        const computerChoice = getComputerChoice();
-        console.log(oneRoundOfGame(userChoice.toLowerCase(), computerChoice))
-    }
-    return winnerOfTheGame(player, computer)
+    computerButton.addEventListener('mouseout', () => {
+        let visiablePop = document.querySelector('.pop-up');
+        visiablePop.style.opacity = '0'
     
+    })
+})
 
+let divResult = document.createElement('div');
+divResult.setAttribute('id', 'result');
+document.querySelector('body').appendChild(divResult)
+let displayingResult = document.querySelector('#result')
+displayingResult.style.marginTop = '30px'
+divResult.style.color = '#fff'
+let scorePlayer = document.querySelector('.player-score')
+let scoreComputer = document.querySelector('.computer-score')
+
+userChoiceDiv.addEventListener('click', eventFun)
+
+function eventFun(event) {
+    const eventTarget = event.target;
+    switch(eventTarget.id) {        
+        case 'rock':
+            divResult.textContent = oneRoundOfGame(eventTarget.textContent.toLowerCase(), getComputerChoice())
+            break;
+
+        case 'paper':   
+            divResult.textContent = oneRoundOfGame(eventTarget.textContent.toLowerCase(), getComputerChoice())
+            break;
+
+        case 'scissors':
+            divResult.textContent = oneRoundOfGame(eventTarget.textContent.toLowerCase(), getComputerChoice())
+    }
+
+    scorePlayer.textContent = `Player: ${player}`
+    scoreComputer.textContent = `Computer: ${computer}`
+    
+    if (player >= 5 || computer >= 5) {
+        divResult.textContent = winnerOfTheGame(player, computer)
+        divResult.style.fontSize = '30px'
+        userChoiceDiv.removeEventListener('click', eventFun)
+    }
 }
 
-// console log here so winner function would be able to show result
-console.log(game())
+
+
+
 
